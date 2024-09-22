49ca69b94f3269eb99ecd72914502388dd5f899a
estebanfallasf <estebanfallasf@gmail.com>
2024-09-21 22:46:25 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,6 +12,7 @@ output "bucket_name_failover" {
 output "bucket_regional_domain_name_failover" {
   value = module.s3Failover.bucket_regional_domain_name
 }
+
 output "bucket_name_primary" {
   value = module.s3Primary.bucket_name
 }
@@ -19,10 +20,6 @@ output "bucket_name_primary" {
 output "bucket_regional_domain_name_primary" {
   value = module.s3Primary.bucket_regional_domain_name
 }
-/*
-output "ec2_public_ipv4_dns" {
-  value = module.vpc_a_ec2_public.public_ip4_dns
-}*/
 
 output "cloudfront_domain_name" {
   value = module.cloud-front.cloudfront_domain_name
